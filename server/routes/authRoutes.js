9b3bf2ba0cc4869c11a76470e5b4e24976c634fd
firedfrog23 <julianlex23@gmail.com>
2024-12-31 20:44:48 +0700

    progress 4: middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { login, logout, register } from '../controllers/authController.js';
+import { login, logout, register, sendVerifyOtp } from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', sendVerifyOtp)
 
 export default authRouter;
