cc298ab6423bbffcb9e0803622fd724fa9bcb168
firedfrog23 <julianlex23@gmail.com>
2025-01-04 22:20:05 +0700

    progress 7: header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen 
+      bg-[url("/bg_img.png")] bg-cover bg-center'>
         <Navbar/>
-      <h1>Home Page</h1>
+        <Header/>
     </div>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,7 @@ const Login = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <Header/>
     </div>
   )
 }
